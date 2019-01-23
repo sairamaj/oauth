f4f0990b0e94988785c79f62d5b38f25862bbadd
Jamalapuram <sairama.Jamalapuram@Fiserv.com>
2019-01-23 14:22:43 -0800

    power shell script added

---
+++ Flows/MachineToMachine(ClientCredentialGrant)/src/scripts/Get-Token.ps1
@@ -0,0 +1,25 @@
+﻿$clientId = "client"
+$ClientSecret = "secret"
+
+<#POST https://api.authorization-server.com/token
+    grant_type=client_credentials&
+    client_id=CLIENT_ID&
+    client_secret=CLIENT_SECRET
+#>
+
+$uri = 'http://localhost:5000/connect/token'
+$body = @{}
+$body["grant_type"] = "client_credentials"
+$body["client_id"] = "client"
+$body["client_secret"] = "secret"
+
+# get access token.
+$response = Invoke-WebRequest -Method Post -Uri $uri -Body $body 
+$tokenInfo = ConvertFrom-Json $response.Content
+$tokenInfo.access_token
+
+# access api with access token.
+$apiUrl = 'http://localhost:5002/api/identity'
+$headers = @{"Authorization"="bearer "+  $tokenInfo.access_token}
+Invoke-RestMethod -Uri $apiUrl -Headers $headers
+
