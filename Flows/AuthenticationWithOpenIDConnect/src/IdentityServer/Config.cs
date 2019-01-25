a0ee98867a3da519c50ff3263b5d17e7ba472831
Sairama <sairamaj@gmail.com>
2019-01-24 17:33:20 -0800

    openid implict

---
+++ Flows/AuthenticationWithOpenIDConnect/src/IdentityServer/Config.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[] { };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+    {
+        // other clients omitted...
+
+        // OpenID Connect implicit flow client (MVC)
+        new Client
+        {
+            ClientId = "mvc",
+            ClientName = "MVC Client",
+            AllowedGrantTypes = GrantTypes.Implicit,
+
+            // where to redirect to after login
+            RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+            AllowedScopes = new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
+            },
+            RequireConsent = true
+        }
+    };
+        }
+    }
+}
